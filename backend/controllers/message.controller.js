97579ecf8d0035e3958322b5fa6b76fe787c271c
NirwanGupta <guptanirwan@gmail.com>
2025-01-01 15:37:11 +0530

    cache the messages - REDIS-IO

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -2,6 +2,7 @@ const User = require(`../models/user.model`);
 const Message = require(`../models/message.model`);
 const {cloudinary} = require(`../db/cloudinary`);
 const { getReceiverSocketId, io } = require("../db/socket");
+const {client} = require(`../redis-client`);    
 
 const getUsersForSidebar = async (req, res) => {
     const loggedInUserId = req.user._id;
@@ -13,12 +14,22 @@ const getMessages = async (req, res) => {
     const {id: receiverId} = req.params;
     const senderId = req.user._id;
     // console.log("senderId: ", senderId, ", receiverId: " , receiverId);
+    console.log("Checking cache");
+    const cacheMessages = await client.get(`Messages-${senderId}-${receiverId}`);
+    if(cacheMessages) {
+        console.log("Found in cache");
+        const messages = JSON.parse(cacheMessages);
+        return res.status(200).json(messages);
+    }
+    console.log("Not found in cache");
     const messages = await Message.find({
         $or: [
             {senderId: senderId, receiverId: receiverId},
             {senderId: receiverId, receiverId: senderId}
         ]
     });
+    await client.set(`Messages-${senderId}-${receiverId}`, JSON.stringify(messages));
+    await client.expire(`Messages-${senderId}-${receiverId}`, 172800);
     res.status(200).json(messages);
 }
 
@@ -41,11 +52,44 @@ const sendMessage = async (req, res) => {
         // console.log("newMessage: ", newMessage);
     }
 
+    let messages = await client.get(`Messages-${senderId}-${receiverId}`);
+    messages = JSON.parse(messages);
+    messages.push(newMessage);
+    await client.expire(`Messages-${senderId}-${receiverId}`, 0);
+    await client.set(`Messages-${senderId}-${receiverId}`, JSON.stringify(messages));
     res.status(200).json(newMessage);
+}
+
+const deleteMessage = async (req, res) => {
+    const {id: messageId} = req.params;
+    const message = await Message.findByIdAndDelete({_id: messageId});
+    let messages = await client.get(`Messages-${senderId}-${receiverId}`);
+    messages = JSON.parse(messages);
+    messages = messages.filter(mes => mes._id !== messageId);
+    await client.expire(`Messages-${senderId}-${receiverId}`, 0);
+    await client.set(`Messages-${senderId}-${receiverId}`, JSON.stringify(messages));
+    res.status(200).json(messages);
+}
+
+const editMessage = async(req, res) => {
+    const {id: messageId} = req.params;
+    const {text} = req.body;
+    const message = await Message.findOne({_id: messageId});
+    message.text = text;
+    message.updatedAt = new Date();
+    await message.save();
+    let messages = await client.get(`Messages-${senderId}-${receiverId}`);
+    messages = JSON.parse(messages);
+    messages = messages.map(mes => mes._id === messageId ? message : mes);
+    await client.expire(`Messages-${senderId}-${receiverId}`, 0);
+    await client.set(`Messages-${senderId}-${receiverId}`, JSON.stringify(messages));
+    res.status(200).json(message);
 }
 
 module.exports = {
     getUsersForSidebar,
     getMessages,
     sendMessage,
+    deleteMessage,
+    editMessage,
 };
